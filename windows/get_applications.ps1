7ff0560d60a49b476de0576484a2298ca8780392
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-29 12:56:39 -0800

    Create get_applications.ps1

---
+++ windows/get_applications.ps1
@@ -0,0 +1,21 @@
+$InstallPaths = @(
+    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall',
+    'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+)
+$CurrentUser = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty UserName
+if ($CurrentUser) {
+    $UserSid = (Get-WmiObject -class win32_useraccount | Where-Object { $_.Caption -eq $CurrentUser }).SID
+    $InstallPaths += if ($UserSid) {
+        [void] (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
+        if (Test-Path -Path HKU:\) {
+            "HKU:\$UserSid\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+            "HKU:\$UserSid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+        }
+    }
+}
+foreach ($Path in $InstallPaths) {
+    if (Test-Path $Path) {
+        Get-ItemProperty -Path $Path -Recurse | Where-Object { $_.DisplayName -and $_.Publisher -and
+        $_.DisplayVersion } | Select-Object DisplayName, DisplayVersion, Publisher | Format-List
+    }
+}
