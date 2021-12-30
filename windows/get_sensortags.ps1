ac4afa6cd28ef8b235e4ebe0ce60259d09e33af2
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-29 16:22:02 -0800

    Create get_sensortags.ps1

---
+++ windows/get_sensortags.ps1
@@ -0,0 +1,6 @@
+$Key = 'HKEY_LOCAL_MACHINE\SYSTEM\CrowdStrike\{9b03c1d9-3138-44ed-9fae-d9f4c034b88d}\' +
+    '{16e0423f-7058-48c9-a204-725362b67639}\Default'
+[PSCustomObject] @{
+    hostname    = [System.Net.Dns]::GetHostname()
+    sensor_tags = "$((((reg query $Key) -match 'GroupingTags') -split 'REG_SZ')[-1].Trim())"
+} | ConvertTo-Json -Compress
