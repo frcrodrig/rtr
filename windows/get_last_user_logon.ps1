7ebcc3f4dc097fd697483c51c4f298dcc17d8261
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:02:55 -0800

    Delete get_last_user_logon.ps1

--- windows/get_last_user_logon.ps1
+++
@@ -1,6 +0,0 @@
-
-[PSCustomObject] @{
-    Hostname         = [System.Net.Dns]::GetHostname()
-    LastUsedUsername = (
-        Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon').LastUsedUsername
-} | ConvertTo-Json -Compress
