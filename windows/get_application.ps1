0e4f02948fee04a5b7bfd7039a14232353250898
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 15:39:15 -0800

    Update get_application.ps1

--- windows/get_application.ps1
+++ windows/get_application.ps1
@@ -5,9 +5,9 @@ $InstallPaths = @(
 $CurrentUser = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty UserName
 if ($CurrentUser) {
     $UserSid = (Get-WmiObject -class win32_useraccount | Where-Object { $_.Caption -eq $CurrentUser }).SID
-    $InstallPaths += if ($UserSid) {
+    if ($UserSid) {
         [void] (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
-        if (Test-Path -Path HKU:\) {
+        $InstallPaths += if (Test-Path -Path HKU:\) {
             "HKU:\$UserSid\Software\Microsoft\Windows\CurrentVersion\Uninstall"
             "HKU:\$UserSid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
         }
