8ef8eac5d0387eefd446ddd2afd724c09df127ad
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:50:28 -0800

    Create get_network_port.ps1

---
+++ windows/get_network_port.ps1
@@ -0,0 +1,21 @@
+$LocalHost = [System.Net.Dns]::GetHostname()
+$Content = @(
+    Get-NetTcpConnection | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State
+    Get-NetUDPEndpoint | Select-Object LocalAddress, LocalPort
+)
+if ($Content) {
+    $Content | ForEach-Object {
+        [PSCustomObject] @{
+            Hostname      = $LocalHost
+            Protocol      = if ($_.State) { 'TCP' } else { 'UDP' }
+            LocalAddress  = $_.LocalAddress
+            LocalPort     = $_.LocalPort
+            RemoteAddress = $_.RemoteAddress
+            RemotePort    = $_.RemotePort
+            State         = $_.State
+
+        } | ConvertTo-Json -Compress
+    }
+} else {
+    Write-Error 'no_port_found'
+}
