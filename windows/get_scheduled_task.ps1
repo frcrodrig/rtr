c394ba9a21fe5063c58fbb8dfa615515b86eaa7d
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:40:24 -0800

    Update get_scheduled_task.ps1

--- windows/get_scheduled_task.ps1
+++ windows/get_scheduled_task.ps1
@@ -1,13 +1,20 @@
-foreach ($Task in (Get-ChildItem -Path "$($env:SYSTEMROOT)\System32\Tasks" -File -Recurse |
-Select-Object Name, FullName)) {
-    [xml] (Get-Content $Task.FullName) | ForEach-Object {
-        [PSCustomObject] @{
-            Task      = $Task.Name
-            UserId    = $_.Task.Principals.Principal.UserId
-            Author    = $_.Task.RegistrationInfo.Author
-            Enabled   = $_.Task.Settings.Enabled
-            Command   = $_.Task.Actions.Exec.Command
-            Arguments = $_.Task.Actions.Exec.Arguments
-        } | Out-String
+$LocalHost = [System.Net.Dns]::GetHostname()
+$Content = Get-ChildItem -Path "$($env:SYSTEMROOT)\System32\Tasks" -File -Recurse |
+    Select-Object Name, FullName
+if ($Content) {
+    foreach ($Task in $Content) {
+        foreach ($Xml in ([xml] (Get-Content $Task.FullName))) {
+            [PSCustomObject] @{
+                Hostname  = $LocalHost
+                Name      = $Task.Name
+                UserId    = $Xml.Task.Principals.Principal.UserId
+                Author    = $Xml.Task.RegistrationInfo.Author
+                Enabled   = $Xml.Task.Settings.Enabled
+                Command   = $Xml.Task.Actions.Exec.Command
+                Arguments = $Xml.Task.Actions.Exec.Arguments
+            } | ConvertTo-Json -Compress
+        }
     }
+} else {
+    Write-Error 'no_scheduled_task_found'
 }
