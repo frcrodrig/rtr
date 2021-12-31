6507b201afa6fbd574a82ef7ffa1dd38cb82787d
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:40:18 -0800

    Update get_local_admin.ps1

--- windows/get_local_admin.ps1
+++ windows/get_local_admin.ps1
@@ -1,8 +1,14 @@
-Get-LocalGroupMember -Group Administrators | ForEach-Object {
-    [PSCustomObject] @{
-        Hostname        = [System.Net.Dns]::GetHostname()
-        ObjectClass     = $_.ObjectClass
-        Name            = $_.Name
-        PrincipalSource = $_.PrincipalSource
-    } | ConvertTo-Json -Compress
+$LocalHost = [System.Net.Dns]::GetHostname()
+$Content = Get-LocalGroupMember -Group Administrators
+if ($Content) {
+    $Content | ForEach-Object {
+        [PSCustomObject] @{
+            Hostname        = $LocalHost
+            ObjectClass     = $_.ObjectClass
+            Name            = $_.Name
+            PrincipalSource = $_.PrincipalSource
+        } | ConvertTo-Json -Compress
+    }
+} else {
+    Write-Error 'no_administrator_found'
 }
