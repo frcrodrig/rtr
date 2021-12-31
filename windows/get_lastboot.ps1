ef8885f7da7d041f694d1e9c4ecf05c57a6983a4
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 10:50:07 -0800

    Delete get_lastboot.ps1

--- windows/get_lastboot.ps1
+++
@@ -1,4 +0,0 @@
-[PSCustomObject] @{
-    Hostname       = [System.Net.Dns]::GetHostname()
-    LastBootUpTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
-} | ConvertTo-Json -Compress
