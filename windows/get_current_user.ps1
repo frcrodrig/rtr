a8c25743d6f0a7e68ee7a3850da04e224f6d0764
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:21:01 -0800

    Update get_current_user.ps1

--- windows/get_current_user.ps1
+++ windows/get_current_user.ps1
@@ -1,10 +1,26 @@
-$ActiveUser = Get-ItemProperty 'HKCU:\Volatile Environment' -ErrorAction SilentlyContinue
-[PSCustomObject] @{
-    Hostname              = [System.Net.Dns]::GetHostname()
-    ActiveUserDomain      = if ($ActiveUser) { $ActiveUser.USERDOMAIN }
-    ActiveUserLogonServer = if ($ActiveUser) { $ActiveUser.LOGONSERVER }
-    ActiveUsername        = if ($ActiveUser) { $ActiveUser.USERNAME }
-    ActiveUserProfile     = if ($ActiveUser) { $ActiveUser.USERPROFILE }
-    LastUsedUsername      = (Get-ItemProperty -Path (
-        'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon')).LastUsedUsername
-} | ConvertTo-Json -Compress
+$LocalHost = [System.Net.Dns]::GetHostname()
+$Winlogon = Get-ItemProperty -Path (
+    'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon') -ErrorAction SilentlyContinue
+$ActiveUser = Get-ItemProperty 'HKCU:\Volatile Environment' -ErrorAction SilentlyContinue |
+    Select-Object USERDOMAIN, LOGONSERVER, USERNAME, USERPROFILE
+if ($ActiveUser) {
+    $ActiveUser | ForEach-Object {
+        [PSCustomObject] @{
+            Hostname              = $LocalHost
+            LastUsedUsername      = $Winlogon.LastUsedUsername
+            ActiveUserDomain      = $_.USERDOMAIN
+            ActiveUserLogonServer = $_.LOGONSERVER
+            ActiveUsername        = $_.USERNAME
+            ActiveUserProfile     = $_.USERPROFILE
+        }
+    }
+} else {
+    [PSCustomObject] @{
+        Hostname              = $LocalHost
+        LastUsedUsername      = $Winlogon.LastUsedUsername
+        ActiveUserDomain      = $null
+        ActiveUserLogonServer = $null
+        ActiveUsername        = $null
+        ActiveUserProfile     = $null
+    }
+}
