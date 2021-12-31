67173216d15412e8589a489c7e6b7d0cba2e24b6
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 12:36:54 -0800

    Update get_remote_share.ps1

--- windows/get_remote_share.ps1
+++ windows/get_remote_share.ps1
@@ -1,15 +1,18 @@
 $LocalHost = [System.Net.Dns]::GetHostname()
-$Content = foreach ($Username in (Get-WmiObject Win32_ComputerSystem).UserName) {
-    $UserSid = (Get-WmiObject Win32_UserAccount | Where-Object { $_.Caption -eq $Username }).SID
-    [void] (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
-    Get-ChildItem -Path "HKU:\$UserSid\Network" | ForEach-Object {
-        Split-Path -Path $_.Name -Leaf | ForEach-Object {
-            [PSCustomObject] @{
-                Hostname   = $LocalHost
-                Sid        = $UserSid
-                Username   = $Username
-                Share      = $_
-                RemotePath = Get-ItemPropertyValue -Path $_ -Name RemotePath
+$Content = foreach ($Username in (Get-WmiObject Win32_ComputerSystem -ErrorAction SilentlyContinue).UserName) {
+    $UserSid = (Get-WmiObject Win32_UserAccount -ErrorAction SilentlyContinue | Where-Object {
+        $_.Caption -eq $Username }).SID
+    if ($UserSid) {
+        [void] (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
+        Get-ChildItem -Path "HKU:\$UserSid\Network" | ForEach-Object {
+            Split-Path -Path $_.Name -Leaf | ForEach-Object {
+                [PSCustomObject] @{
+                    Hostname   = $LocalHost
+                    Sid        = $UserSid
+                    Username   = $Username
+                    Share      = $_
+                    RemotePath = Get-ItemPropertyValue -Path $_ -Name RemotePath
+                }
             }
         }
     }
