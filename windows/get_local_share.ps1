a33248762b9e0a58f776c03cec35a6f9adac46f6
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 12:17:49 -0800

    Create get_local_share.ps1

---
+++ windows/get_local_share.ps1
@@ -0,0 +1,15 @@
+$LocalHost = [System.Net.Dns]::GetHostname()
+$Content = Get-SmbShare
+if ($Content) {
+    $Content | ForEach-Object {
+        [PSCustomObject] @{
+            Hostname    = $LocalHost
+            Name        = $_.Name
+            ScopeName   = $_.ScopeName
+            Path        = $_.Path
+            Description = $_.Description
+        } | ConvertTo-Json -Compress
+    }
+} else {
+    Write-Error 'no_local_share_found'
+}
