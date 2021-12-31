f00890e9afb17a8f8f48c1ad857c511d7f64158d
bk-cs <54042976+bk-cs@users.noreply.github.com>
2021-12-31 11:40:19 -0800

    Update get_monitor.ps1

--- windows/get_monitor.ps1
+++ windows/get_monitor.ps1
@@ -1,13 +1,18 @@
 $LocalHost = [System.Net.Dns]::GetHostname()
-Get-WmiObject -Namespace root\wmi -Class WmiMonitorID | Select-Object ManufacturerName, UserFriendlyName,
-SerialNumberID | ForEach-Object {
-    $_.PSObject.Properties | Where-Object { $_.Value -is [System.Array] } | ForEach-Object {
-        $_.Value = ([System.Text.Encoding]::ASCII.GetString($_.Value -notmatch 0))
+$Content = Get-WmiObject -Namespace root\wmi -Class WmiMonitorID |
+    Select-Object ManufacturerName, UserFriendlyName, SerialNumberID
+if ($Content) {
+    $Content | ForEach-Object {
+        $_.PSObject.Properties | Where-Object { $_.Value -is [System.Array] } | ForEach-Object {
+            $_.Value = ([System.Text.Encoding]::ASCII.GetString($_.Value -notmatch 0))
+        }
+        [PSCustomObject] @{
+            Hostname         = $LocalHost
+            ManufacturerName = $_.ManufacturerName
+            UserFriendlyName = $_.UserFriendlyName
+            SerialNumberID   = $_.SerialNumberID
+        } | ConvertTo-Json -Compress
     }
-    [PSCustomObject] @{
-        Hostname         = $LocalHost
-        ManufacturerName = $_.ManufacturerName
-        UserFriendlyName = $_.UserFriendlyName
-        SerialNumberID   = $_.SerialNumberID
-    } | ConvertTo-Json -Compress
+} else {
+    Write-Error 'no_monitor_found'
 }
