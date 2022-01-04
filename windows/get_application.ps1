89698f08c23ab6b22ae0768257115aec47df428d
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-04 10:18:00 -0800

    Update get_application.ps1

--- windows/get_application.ps1
+++ windows/get_application.ps1
@@ -1,24 +1,13 @@
-$InstallPaths = @(
-    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall',
-    'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
-)
-$CurrentUser = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty UserName
-if ($CurrentUser) {
-    $UserSid = (Get-WmiObject -class win32_useraccount | Where-Object { $_.Caption -eq $CurrentUser }).SID
-    if ($UserSid) {
-        [void] (New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS)
-        $InstallPaths += if (Test-Path -Path HKU:\) {
-            "HKU:\$UserSid\Software\Microsoft\Windows\CurrentVersion\Uninstall"
-            "HKU:\$UserSid\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
-        }
-    }
-}
 $LocalHost = [System.Net.Dns]::GetHostname()
-$Content = foreach ($Path in $InstallPaths) {
-    if (Test-Path $Path) {
-        Get-ItemProperty -Path "$Path\*" -ErrorAction SilentlyContinue |
-            Where-Object { $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } |
-            Select-Object DisplayName, DisplayVersion, Publisher
+$Content = ('Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+'Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*').foreach{
+    Get-ItemProperty -Path "Registry::\HKEY_LOCAL_MACHINE\$_" -ErrorAction SilentlyContinue | Where-Object {
+        $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } | Select-Object DisplayName, DisplayVersion,
+        Publisher
+    foreach ($UserSid in (Get-WmiObject Win32_UserProfile | Where-Object { $_.SID -like 'S-1-5-21-*' }).SID) {
+        Get-ItemProperty -Path "Registry::\HKEY_USERS\$UserSid\$_" -ErrorAction SilentlyContinue | Where-Object {
+            $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } | Select-Object DisplayName, DisplayVersion,
+            Publisher
     }
 }
 if ($Content) {
