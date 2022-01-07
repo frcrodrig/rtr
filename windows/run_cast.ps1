af42b48eb1726a1418ff6f8c1e400483d5943b44
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-07 14:04:09 -0800

    Update run_cast.ps1

--- windows/run_cast.ps1
+++ windows/run_cast.ps1
@@ -1,33 +1,32 @@
-$CastPath = 'C:\cast.exe'
-if ((Test-Path $CastPath) -eq $false) {
-    throw "'cast.exe' not found. Use 'put' to deliver it to '$CastPath' before continuing."
+if ((Test-Path 'C:\cast.exe') -eq $false) {
+    throw "'cast.exe' not found. Use 'put' to deliver it to 'C:\' before continuing."
 }
 $ScriptBlock = {
-    $JsonPath = 'C:\cast.json'
-    Start-Process $CastPath -ArgumentList 'scan C:\' -RedirectStandardOutput $JsonPath -PassThru | ForEach-Object {
+    Start-Process 'C:\cast.exe' -ArgumentList 'scan C:\' -RedirectStandardOutput 'C:\cast.json' -PassThru |
+    ForEach-Object {
         if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) {
-            Send-ToHumio (@{ Started = $CastPath })
+            Send-ToHumio (@{ Started = 'C:\cast.exe' })
         }
         Wait-Process $_.Id
-        if ((Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) -and (Test-Path $JsonPath)) {
-            (Get-Content $JsonPath).Normalize() | ForEach-Object {
+        if ((Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) -and (Test-Path 'C:\cast.json')) {
+            (Get-Content 'C:\cast.json').Normalize() | ForEach-Object {
                 Send-ToHumio @($_)
-                Remove-Item -Path $JsonPath
-                $JsonStatus = if (Test-Path -Path $JsonPath) { 'Failed_To_Remove' } else { 'Removed' }
-                Send-HumioEvent (@{ $JsonStatus = $JsonPath })
+                Remove-Item -Path 'C:\cast.json'
+                $JsonStatus = if (Test-Path -Path 'C:\cast.json') { 'Failed_To_Remove' } else { 'Removed' }
+                Send-HumioEvent (@{ $JsonStatus = 'C:\cast.json' })
             }
         }
-        if (Test-Path $CastPath) {
-            Remove-Item -Path $CastPath
+        if (Test-Path 'C:\cast.exe') {
+            Remove-Item -Path 'C:\cast.exe'
             if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) { 
-                $CastStatus = if (Test-Path -Path $CastPath) { 'Failed_To_Remove' } else { 'Removed' }
-                Send-HumioEvent (@{ $CastStatus = $CastPath })
+                $CastStatus = if (Test-Path -Path 'C:\cast.exe') { 'Failed_To_Remove' } else { 'Removed' }
+                Send-HumioEvent (@{ $CastStatus = 'C:\cast.exe' })
             }
         }
     }
 }
-$ArgumentList = '-Command &{' + $ScriptBlock + '} -CastPath ' + $CastPath
-Start-Process -FilePath powershell.exe -ArgumentList $ArgumentList -PassThru | ForEach-Object {
+Start-Process -FilePath powershell.exe -ArgumentList '-Command &{' + $ScriptBlock + '}' -PassThru |
+ForEach-Object {
     [PSCustomObject] @{
         Host        = [System.Net.Dns]::GetHostname()
         Id          = $_.Id
