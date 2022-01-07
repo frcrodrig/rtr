27caf404f63e86c1457f2ea0621f8affd89e4c04
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-07 14:47:00 -0800

    Update run_cast.ps1

--- windows/run_cast.ps1
+++ windows/run_cast.ps1
@@ -1,19 +1,21 @@
 $ScriptBlock = {
     if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) {
-        Send-ToHumio (@{ Started = 'C:\cast.exe' })
-    }
-    $Scan = C:\cast.exe scan C:\ *>&1
-    if ($Scan -and (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue)) {
-        Send-ToHumio @($Scan)
-    } elseif ($Scan) {
-        $Scan >> C:\cast.json
+        Send-ToHumio (C:\cast.exe scan C:\ *>&1)
+    } else {
+        $Param = @{
+            FilePath               = 'C:\cast.exe'
+            ArgumentList           = 'scan C:\'
+            RedirectStandardOutput = 'C:\cast.json'
+            RedirectStandardError  = 'C:\cast.json'
+        }
+        Start-Process @Param
     }
     if (Test-Path 'C:\cast.exe') {
         Remove-Item -Path 'C:\cast.exe'
-        if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) { 
-            $ExeStatus = if (Test-Path -Path 'C:\cast.exe') { 'Failed_To_Remove' } else { 'Removed' }
-            Send-HumioEvent (@{ $ExeStatus = 'C:\cast.exe' })
-        }
+    }
+    if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) { 
+        $ExeStatus = if (Test-Path -Path 'C:\cast.exe') { 'Failed_To_Remove' } else { 'Removed' }
+        Send-ToHumio (@{ $ExeStatus = 'C:\cast.exe' })
     }
 }
 if ((Test-Path 'C:\cast.exe') -eq $false) {
