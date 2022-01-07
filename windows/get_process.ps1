d52c51f5643f58b5f8893d08782eb6fd48a967bf
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-07 10:44:35 -0800

    Update get_process.ps1

--- windows/get_process.ps1
+++ windows/get_process.ps1
@@ -1,18 +1,27 @@
 $LocalHost = [System.Net.Dns]::GetHostname()
 $Content = Get-Process -ErrorAction SilentlyContinue | Select-Object Name, Id, StartTime, WorkingSet, CPU,
-    HandleCount, Path
-if ($Content) {
-    $Content | Where-Object { $_ } | ForEach-Object {
-        [PSCustomObject] @{
-            Hostname    = $LocalHost
-            Id          = $_.Id
-            Name        = $_.Name
-            StartTime   = $_.StartTime
-            WorkingSet  = $_.WorkingSet
-            CPU         = $_.CPU
-            HandleCount = $_.HandleCount
-            Path        = $_.Path
-        } | ConvertTo-Json -Compress
+HandleCount, Path | ForEach-Object {
+    [PSCustomObject] @{
+        Hostname    = $LocalHost
+        Id          = $_.Id
+        Name        = $_.Name
+        StartTime   = $_.StartTime
+        WorkingSet  = $_.WorkingSet
+        CPU         = $_.CPU
+        HandleCount = $_.HandleCount
+        Path        = $_.Path
+    }
+}
+if ($Content -and (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue)) {
+    SendTo-Humio $Content
+    ConvertTo-Json -InputObject ([PSCustomObject] @{
+        host   = $LocalHost
+        script = 'get_process.ps1'
+        message = 'check_humio_for_result'
+    }) -Compress
+} elseif ($Content) {
+    @($Content).foreach{
+        ConvertTo-Json -InputObject $_ -Compress
     }
 } else {
     Write-Error 'no_process_found'
