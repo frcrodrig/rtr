76b2d91f6f6fa78c407ceaf1ed9fe8fa40bc0dcc
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-07 14:14:22 -0800

    Update run_cast.ps1

--- windows/run_cast.ps1
+++ windows/run_cast.ps1
@@ -1,6 +1,3 @@
-if ((Test-Path 'C:\cast.exe') -eq $false) {
-    throw "'cast.exe' not found. Use 'put' to deliver it to 'C:\' before continuing."
-}
 $ScriptBlock = {
     Start-Process 'C:\cast.exe' -ArgumentList 'scan C:\' -RedirectStandardOutput 'C:\cast.json' -PassThru |
     ForEach-Object {
@@ -11,21 +8,24 @@ $ScriptBlock = {
         if ((Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) -and (Test-Path 'C:\cast.json')) {
             (Get-Content 'C:\cast.json').Normalize() | ForEach-Object {
                 Send-ToHumio @($_)
-                Remove-Item -Path 'C:\cast.json'
-                $JsonStatus = if (Test-Path -Path 'C:\cast.json') { 'Failed_To_Remove' } else { 'Removed' }
-                Send-HumioEvent (@{ $JsonStatus = 'C:\cast.json' })
             }
+            Remove-Item -Path 'C:\cast.json'
+            $JsonStatus = if (Test-Path -Path 'C:\cast.json') { 'Failed_To_Remove' } else { 'Removed' }
+            Send-HumioEvent (@{ $JsonStatus = 'C:\cast.json' })
         }
         if (Test-Path 'C:\cast.exe') {
             Remove-Item -Path 'C:\cast.exe'
             if (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue) { 
-                $CastStatus = if (Test-Path -Path 'C:\cast.exe') { 'Failed_To_Remove' } else { 'Removed' }
-                Send-HumioEvent (@{ $CastStatus = 'C:\cast.exe' })
+                $ExeStatus = if (Test-Path -Path 'C:\cast.exe') { 'Failed_To_Remove' } else { 'Removed' }
+                Send-HumioEvent (@{ $ExeStatus = 'C:\cast.exe' })
             }
         }
     }
 }
-Start-Process -FilePath powershell.exe -ArgumentList '-Command &{' + $ScriptBlock + '}' -PassThru |
+if ((Test-Path 'C:\cast.exe') -eq $false) {
+    throw "'cast.exe' not found. Use 'put' to deliver it to 'C:\' before continuing."
+}
+Start-Process -FilePath powershell.exe -ArgumentList "-Command &{$ScriptBlock}" -PassThru |
 ForEach-Object {
     [PSCustomObject] @{
         Host        = [System.Net.Dns]::GetHostname()
