4e5bc09d9539953db4f6e93fbd50f42b503d2356
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-08 11:36:09 -0800

    Formatting
    Replaced commands with aliases, and removed whitespace to reduce script size for transmission from library
    
    Updated to include additional adapter properties, including IPv4 and IPv6 addresses.

--- windows/get_network_adapter.ps1
+++ windows/get_network_adapter.ps1
@@ -1,25 +1,13 @@
-$LocalHost = [System.Net.Dns]::GetHostname()
-$Content = Get-NetAdapter -ErrorAction SilentlyContinue | Select-Object Name, InterfaceDescription,
-MacAddress, Status | ForEach-Object {
-    [PSCustomObject] @{
-        Host                 = $LocalHost
-        Name                 = $_.Name
-        InterfaceDescription = $_.InterfaceDescription
-        MacAddress           = $_.MacAddress
-        Status               = $_.Status
-    }
-}
-if ($Content -and (Get-Command -Name Send-ToHumio -ErrorAction SilentlyContinue)) {
-    Send-ToHumio $Content
-    ConvertTo-Json -InputObject ([PSCustomObject] @{
-        Host    = $LocalHost
-        Script  = 'get_network_adapter.ps1'
-        Message = 'check_humio_for_result'
-    }) -Compress
-} elseif ($Content) {
-    $Content | ForEach-Object {
-        $_ | ConvertTo-Json -Compress
-    }
-} else {
-    Write-Error 'no_network_adapter_found'
-}
+$Obj=Get-NetAdapter -EA 0|%{$Ip=Get-NetIpAddress -InterfaceIndex $_.IfIndex|select IPAddress,AddressFamily;
+    [PSCustomObject]@{Name=$_.Name;IfIndex=$_.IfIndex;MacAddress=$_.MacAddress;LinkSpeed=$_.LinkSpeed;
+    Virtual=$_.Virtual;Status=$_.Status;MediaConnectionState=$_.MediaConnectionState;FullDuplex=$_.FullDuplex;
+    DriverName=$_.DriverName;DriverVersionString=$_.DriverVersionString;Ipv4Address=($Ip|?{$_.AddressFamily -eq
+    'IPv4'}).IPAddress;Ipv6Address=($Ip|?{$_.AddressFamily -eq 'IPv6'}).IPAddress}}
+$Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_network_adapter.ps1';
+    Message='no_network_adapter'}
+if(gcm shumio -EA 0){
+    if($Obj){shumio $Obj;$Out|%{$_.Message='check_humio';$_|ConvertTo-Json -Compress}
+    }else{$Out|%{shumio ($_|select Script, Message);Write-Error $_.Message}}
+}elseif($Obj){
+    $Obj|%{$_.PSObject.Properties.Add((New-Object PSNoteProperty('Host',$Out.Host)));$_|ConvertTo-Json -Compress}
+}else{Write-Error $Out.Message}
