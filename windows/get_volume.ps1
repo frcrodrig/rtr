6a4e047f95ceda7286ff3bef1bceb13f1a37d180
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-08 13:30:43 -0800

    Update get_volume.ps1

--- windows/get_volume.ps1
+++ windows/get_volume.ps1
@@ -9,7 +9,7 @@ $StrBld=New-Object System.Text.StringBuilder(65535)
 $K32=Add-Type -MemberDefinition $Def -Name Kernel32 -Namespace Win32 -PassThru
 $Obj=Get-Volume -EA 0|select DriveLetter,FileSystemLabel,FileSystem,SizeRemaining|%{[PSCustomObject]@{
     DriveLetter=$_.DriveLetter;FileSystemLabel=$_.FileSystemLabel;FileSystem=$_.FileSystem;
-    SizeRemaining=$_.SizeRemaining;NtPath=$K32::QueryDosDevice("$($_.DriveLetter):",$StrBld,255)}}
+    SizeRemaining=$_.SizeRemaining;NtPath=($K32::QueryDosDevice("$($_.DriveLetter):",$StrBld,255)).ToString()}}
 $Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_volume.ps1';Message='no_volume'}
 if(gcm shumio -EA 0){
     if($Obj){shumio $Obj;$Out|%{$_.Message='check_humio';$_|ConvertTo-Json -Compress}
