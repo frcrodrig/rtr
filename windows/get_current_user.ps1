f04de7580c8cfdcdc86e97ed557f85c6b5c45dbe
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-08 13:18:24 -0800

    Bugfix
    Updated 'no active user' output

--- windows/get_current_user.ps1
+++ windows/get_current_user.ps1
@@ -2,8 +2,9 @@ $Last=(gp -Path ('HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon') 
 $Active=gp 'HKCU:\Volatile Environment' -EA 0|select USERDOMAIN,LOGONSERVER,USERNAME,USERPROFILE
 $Obj=if($Active){$Active|%{[PSCustomObject]@{LastUsedUsername=$Last;ActiveUserDomain=$_.USERDOMAIN;
     ActiveUserLogonServer=$_.LOGONSERVER;ActiveUsername=$_.USERNAME;ActiveUserProfile=$_.USERPROFILE}}
-}else{[PSCustomObject]@{LastUsedUsername=$Last}|%{@('ActiveUserDomain','ActiveUserLogonServer','ActiveUsername',
-    'ActiveUserProfile').foreach{$_.PSObject.Properties.Add((New-Object PSNoteProperty($_,$null)))};$_}
+}else{$None=[PSCustomObject]@{LastUsedUsername=$Last}|%{@('ActiveUserDomain','ActiveUserLogonServer',
+    'ActiveUsername','ActiveUserProfile').foreach{$None.PSObject.Properties.Add((New-Object PSNoteProperty($_,
+    $null)))};$None}
 }
 $Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_current_user.ps1';Message='check_humio'}
 if(gcm shumio -EA 0){shumio $Obj;$Out|%{$_|ConvertTo-Json -Compress}
