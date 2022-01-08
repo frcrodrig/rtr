9c51ea3295e5a96af36ccdead1ac4a7d8daeeab4
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-08 13:36:38 -0800

    Update get_powershell_env.ps1

--- windows/get_powershell_env.ps1
+++ windows/get_powershell_env.ps1
@@ -6,6 +6,4 @@ $Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_powershe
 if(gcm shumio -EA 0){
     if($Obj){shumio $Obj;$Out|%{$_.Message='check_humio';$_|ConvertTo-Json -Compress}
     }else{$Out|%{shumio ($_|select Script, Message);Write-Error $_.Message}}
-}elseif($Obj){
-    $Obj|%{$_.PSObject.Properties.Add((New-Object PSNoteProperty('Host',$Out.Host)));$_|ConvertTo-Json -Compress}
-}else{Write-Error $Out.Message}
+}elseif($Obj){$Obj|ConvertTo-Json -Compress}else{Write-Error $Out.Message}
