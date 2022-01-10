adbe20ed0917c91e6e16900bd77e9d1225057987
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-10 12:46:05 -0800

    Update get_process.ps1

--- windows/get_process.ps1
+++ windows/get_process.ps1
@@ -1,6 +1,6 @@
 $Obj=Get-Process -EA 0|select Name,Id,StartTime,WorkingSet,CPU,HandleCount,Path|%{
-[PSCustomObject]@{Id=$_.Id;Name=$_.Name;StartTime=$_.StartTime.ToFileTimeUtc();WorkingSet=$_.WorkingSet;
-CPU=$_.CPU;HandleCount=$_.HandleCount;Path=$_.Path}}
+[PSCustomObject]@{Id=$_.Id;Name=$_.Name;StartTime=if($_.StartTime){$_.StartTime.ToFileTimeUtc()}else{$null};
+WorkingSet=$_.WorkingSet;CPU=$_.CPU;HandleCount=$_.HandleCount;Path=$_.Path}}
 $Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_process.ps1';Message='no_process'}
 if(gcm shumio -EA 0){
 if($Obj){shumio $Obj;$Out|%{$_.Message='check_humio';$_|ConvertTo-Json -Compress}
