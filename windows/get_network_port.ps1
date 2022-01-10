b2b399de457b7e3b6d152d51f6755e702ebb5561
bk-cs <54042976+bk-cs@users.noreply.github.com>
2022-01-10 08:08:50 -0800

    Update get_network_port.ps1

--- windows/get_network_port.ps1
+++ windows/get_network_port.ps1
@@ -1,7 +1,8 @@
-$Obj=@(@(Get-NetTcpConnection -EA 0|select LocalAddress,LocalPort,RemoteAddress,RemotePort,State)+
+$Obj=@(@(Get-NetTcpConnection -EA 0|select LocalAddress,LocalPort,RemoteAddress,RemotePort,State,OwningProcess)+
 @(Get-NetUDPEndpoint -EA 0|select LocalAddress,LocalPort))
 $Obj=$Obj|%{[PSCustomObject]@{Protocol=if($_.State){'TCP'}else{'UDP'};LocalAddress=$_.LocalAddress;
-LocalPort=$_.LocalPort; RemoteAddress=$_.RemoteAddress;RemotePort=$_.RemotePort;State=$_.State}}
+LocalPort=$_.LocalPort;RemoteAddress=$_.RemoteAddress;RemotePort=$_.RemotePort;State=$_.State;
+OwningProcess=$_.OwningProcess}}
 $Out=[PSCustomObject]@{Host=[System.Net.Dns]::GetHostname();Script='get_network_port.ps1';
 Message='no_network_port'}
 if(gcm shumio -EA 0){
